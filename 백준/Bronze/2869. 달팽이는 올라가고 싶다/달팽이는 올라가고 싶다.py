4ae40effcae6dcd5f90d7b3fd3c03d7a7122e84c
vanessa cheong <tomoyo519@naver.com>
2023-08-12 20:53:35 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -3,11 +3,10 @@ up , down, meter = map(int, input().split())
 day = up - down;
 almost = meter - up;
 
-countDay = 1;
+countDay = 0;
 
 if( almost % day != 0  ):
-    countDay = (almost // day) +1;
-    countDay +=  1
+    countDay = (almost // day) +2
     
 else:
     countDay = (almost // day) +1
