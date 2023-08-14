766fa91fad894262e6a18330d92738c8e86a544c
vanessa cheong <tomoyo519@naver.com>
2023-08-14 11:48:37 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9192 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,7 +1,8 @@
 import sys
 count = int(sys.stdin.readline())
 
-arr = [0] * 10001
+arr = [0 for i in range(10001)]
+# list = [0 for i in range(n)] 이거랑 무슨 차이 ??
 
 for i in range(count):
     number = int(sys.stdin.readline())
@@ -9,5 +10,6 @@ for i in range(count):
     
 for j in range(10001):
     if(arr[j]>0):
+        # 그냥 print(j)하면 시간초과 나는 이유 ???
         for k in range(arr[j]):
             print(j)
