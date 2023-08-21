b2ee9f10d6990cd3e4eb3bcf31e64e43ccd774e1
gabriela cheong <tomoyo519@naver.com>
2023-08-21 15:48:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 664 ms, Memory: 65092 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -4,21 +4,41 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 node, line = map(int, input().split())
 graph = [[] for _ in range(node+1)]
-for i in range(line):
+for _ in range(line):
     a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-dfs_visited = [False] * (node + 1)
-def dfs(start):
-    dfs_visited[start] = True;
-    for i in graph[start]:
-        if not dfs_visited[i]:
-            dfs(i)
+# dfs_visited = [False] * (node + 1)
+# def dfs(start):
+#     dfs_visited[start] = True;
+#     for i in graph[start]:
+#         if not dfs_visited[i]:
+#             dfs(i)
             
-count = 0
+# count = 0
+# for i in range(1, node+1):
+#     if not dfs_visited[i]:
+#         dfs(i)
+#         count+=1
+# print(count)
+
+bfs_visited = [False] * (node+1)
+def bfs(start):
+    queue = deque([start])
+    bfs_visited[start] = True;
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not bfs_visited[i]:
+                
+                bfs_visited[i] = True;
+                queue.append(i);
+                
+count = 0;
 for i in range(1, node+1):
-    if not dfs_visited[i]:
-        dfs(i)
-        count+=1
+    if not bfs_visited[i]:
+
+        bfs(i)
+        count += 1;
 print(count)
