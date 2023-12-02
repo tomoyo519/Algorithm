eab628a6e7620ef408a000de7757b03ec09fb5fb
정다희 <tomoyo519@naver.com>
2023-12-02 23:21:13 +0900

    [Silver II] Title: 에디터, Time: 276 ms, Memory: 43492 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,3 +1,4 @@
+# 스택으로 다시 풀어보기
 import sys
 input = sys.stdin.readline
 # a b c d
@@ -7,19 +8,20 @@ words =list(input()[:-1])
 stack = deque();
 
 count = int(input()); # 4
+# 왼쪽은  커서 왼쪽의 내용 words
+# 오른쪽은 커서 오른쪽의 내용. stack
+for i in range(count):
 
-for _ in range(count):
-    order = input().split()
-    if order[0] == 'L' and words:
-        stack.appendleft(words.pop())
-    elif order[0] == 'D' and stack:
+    order = input().split();
+    
+    if(order[0] == 'L' and words):
+        stack.appendleft(words.pop());
+    elif(order[0] == 'B' and words): 
+        # 4 4
+        words.pop();
+    elif(order[0] == "D" and stack):
         words.append(stack.popleft())
-    elif order[0] == 'B' and words:
-        words.pop()
-    elif order[0] == 'P':
-        words.append(order[1])
-
-for values in words:
-    print(values,end='')
-for values in stack:
-    print(values,end='')
+    elif(order[0] == "P" ):
+        words.append(order[1]);
+result = words + list(stack);
+print("".join(result));
