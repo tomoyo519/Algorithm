7edb6eec4208e275fe808e2fdce15b9f4635395d
정다희 <tomoyo519@naver.com>
2023-12-22 22:31:31 +0900

    [Bronze II] Title: 블랙잭, Time: 84 ms, Memory: 37492 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,19 +1,15 @@
-# 3장을 뽑아서 만들 수 있는 합의 경우 중에서, 만들어야 되는 수랑 가장 차이가 작은것 (음수눈 안됌)
+import sys
+input = sys.stdin.readline
 
-import itertools;
-
-cardNum, goalNum = map(int,input().split())
-cardList = []
-
-cardList = (input().split())
-
-gap = []
-nPr = list(itertools.permutations(cardList, 3))
-for k in nPr:
-    total = 0;
-    total = int(k[0]) + int(k[1]) + int(k[2]);
-
-    if(goalNum - total >= 0):
-        gap.append(total)
-
-print(max(gap))
+cards, sum = map(int, input().split())
+array = list(map(int, input().split()));
+numList = [];
+for i in range(cards):
+    for j in range(i+1, cards):
+        for k in range(j+1, cards):
+            three = array[i]+ array[j]+ array[k]
+            if three > sum:
+                continue;
+            else:
+                numList.append(three);
+print(max(numList));
