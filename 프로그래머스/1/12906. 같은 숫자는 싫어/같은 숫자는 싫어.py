addf4caebeb958dd8b9dbe5e3e49a70d8b119738
정다희 <tomoyo519@naver.com>
2023-12-31 19:38:07 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 89.00 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+def solution(arr):
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    curr = arr[0]
+    result = []
+    result.append(curr)
+    for i in range(1, len(arr)):
+
+        if arr[i] == curr:
+            curr = arr[i]
+            continue;
+        else:
+            curr = arr[i]
+            result.append(arr[i])
+    return result            
