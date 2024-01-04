8598efe1cde339753653c080153e351518debbea
정다희 <tomoyo519@naver.com>
2024-01-04 23:47:34 +0900

    [level 1] Title: 모의고사, Time: 4.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,30 +1,19 @@
 def solution(answers):
-    answer = []
     a = [1,2,3,4,5]
     b = [2,1,2,3,2,4,2,5]
     c = [3,3,1,1,2,2,4,4,5,5]
     numA=numB=numC = 0
     for i , j in enumerate(answers):
-        if i >= 5: 
-            i = i % 5;
-        if j == a[i]:
+        if j == a[i%5]:
             numA+=1    
-    for i , j in enumerate(answers):
-        if i >= 8: 
-            i = i % 8;
-        if j == b[i]:
+        if j == b[i%8]:
             numB+=1 
-    for i , j in enumerate(answers):
-        if i >= 10: 
-            i = i % 10;
-        if j == c[i]:
+        if j == c[i%10]:
             numC+=1 
     result = [numA, numB, numC]
-    maxResult = max(result)
     data= []
-    print(result)
     for i in range(len(result)):
-        if result[i] == maxResult:
+        if result[i] == max(result):
             data.append(i+1)
             
     return data
