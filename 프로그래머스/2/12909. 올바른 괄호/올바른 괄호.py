5730907b88f4e16476c9330c14b41f0bea5a66aa
정다희 <tomoyo519@naver.com>
2024-02-07 23:21:34 +0900

    [level 2] Title: 올바른 괄호, Time: 14.01 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(s):
+    data = deque(s)
+    stack = deque()
+    
+    while data:
+        current = data.popleft()
+        if stack and stack[-1] == '(' and current == ')':
+            stack.pop()
+        else:
+            stack.append(current)
+
+
+    if len(stack) == 0:
+        return True;
+    else:
+        return False
+        
+        
