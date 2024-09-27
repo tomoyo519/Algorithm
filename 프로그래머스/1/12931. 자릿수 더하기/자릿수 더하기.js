0fa3bd23a139778ebdfcfd919adec7eef51d5743
정다희 <tomoyo519@naver.com>
2024-09-27 10:21:58 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,12 +1,6 @@
 function solution(n)
 {
-    let answer  = 0;
    let arr = n.toString().split('')
-   for(let i=0; i<arr.length; i++){
-       if(Number(arr[i]) ===NaN) continue;
-       else{
-           answer+=Number(arr[i])
-       }
-   }
+   let answer = arr.reduce((acc,cur)=>Number(acc)  +Number(cur) , 0)
    return answer
 }
