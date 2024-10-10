84c1e4132a69ff620b7abf0c9763c2d041d669b8
정다희 <tomoyo519@naver.com>
2024-10-10 12:29:24 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.10 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,15 +1,14 @@
 function solution(arr1, arr2) {
-    let answer = [];
-
+    var answer = [];
     for(let i=0; i<arr1.length; i++){
-        let newArr = []
-        for(let j=0; j<arr1[i].length ; j++){
-
-            newArr.push(arr1[i][j] + arr2[i][j])
-            
- 
+         let newArr = []
+        for(let j=0; j<arr1[i].length; j++){
+       
+        newArr.push(arr1[i][j]+arr2[i][j])
+          
         }
-                   answer.push(newArr)
+      answer.push(newArr)
+        
     }
-    return answer;
+   return answer
 }
